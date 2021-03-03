39194d757ea1a37dc3f517c96a2520f9edbdd9df
Luke Payne <luke.payne@conduent.com>
2021-03-03 18:54:44 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileSizeSpelunker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the location of the files we're iterating over
+            var path = txtFilePath.Text.Trim();
+
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("Directory does not exist");
+                return;
+            }
+
+
+            DirectoryInfo d = new DirectoryInfo(path);
+
+            List<long> fileSizes = new List<long>();
+            long totalSizeOfAllFiles = 0;
+
+            var extension = txtFileExtension.Text.Trim();
+
+            if (!String.IsNullOrEmpty(extension))
+            {
+                if (extension.Substring(0,1) != ".")
+                {
+                    MessageBox.Show("File extension should start w/ a period - .");
+                    return;
+                }
+
+                extension = "*" + extension;
+            }
+            else // If nothing is put in the extension, search all files
+            {
+                extension = "*";
+            }
+
+
+            foreach (var file in d.GetFiles(extension))
+            {
+                fileSizes.Add(file.Length);
+                totalSizeOfAllFiles += file.Length;
+            }
+
+            if (fileSizes.Count == 0)
+            {
+                MessageBox.Show("No files found.");
+                return;
+            }
+
+            var averageSize = totalSizeOfAllFiles / fileSizes.Count;
+            txtAverageFileSize.Text = averageSize.ToString("N0");
+        }
+    }
+}
